304eeb7b20644d873af36cd7aeaa976bd4de3e85
raudbrand <lysenkov.v.a@yandex.ru>
2026-09-02 20:14:11 +0300

    add back button for employee details

--- apps/web/app/(private)/(employees)/ui/employee-details/employee-details.tsx
+++ apps/web/app/(private)/(employees)/ui/employee-details/employee-details.tsx
@@ -13,6 +13,11 @@ import {
   getEmployeeDetailsError,
 } from "@/Employees/model/selectors/employee-details.selectors"
 import { getEmployeeByIdThunk } from "@/Employees/model/thunks/get-employee-by-id.thunk"
+import { ButtonGroup } from "@workspace/ui/components/button-group"
+import { Button } from "@workspace/ui/components/button"
+import { IconArrowLeft } from "@tabler/icons-react"
+import { Separator } from "@workspace/ui/components/separator"
+import { useRouter } from "next/navigation"
 
 export interface EmployeeDetailsProps {
   employeeId?: string
@@ -25,6 +30,8 @@ export const EmployeeDetails = memo((props: EmployeeDetailsProps) => {
   const data = useAppSelector(getEmployeeDetailsData)
   const error = useAppSelector(getEmployeeDetailsError)
 
+  const router = useRouter()
+
   useEffect(() => {
     if (employeeId) dispatch(getEmployeeByIdThunk({ employeeId: employeeId }))
   }, [dispatch, employeeId])
@@ -34,19 +41,36 @@ export const EmployeeDetails = memo((props: EmployeeDetailsProps) => {
       reducers={{ employeeDetailsSchema: employeeDetailsReducer }}
       removeAfterUnmount={true}
     >
-      <div className={"flex w-full flex-col items-start justify-start gap-3"}>
-        {error && <div>{error}</div>}
-        <div
-          className={"flex w-full flex-row items-center justify-center gap-4"}
-        >
-          <Avatar style={{ width: 100, height: 100 }} />
+      <div className={"flex w-full flex-col gap-4"}>
+        <ButtonGroup>
+          <ButtonGroup>
+            <Button
+              variant="outline"
+              aria-label="Go Back"
+              onClick={() => router.back()}
+            >
+              <IconArrowLeft />
+              {"Назад"}
+            </Button>
+          </ButtonGroup>
+        </ButtonGroup>
+        <Separator />
+        <div className={"flex w-full flex-col items-start justify-start gap-3"}>
+          {error && <div>{error}</div>}
           <div
-            className={"flex w-full flex-col items-start justify-center gap-1"}
+            className={"flex w-full flex-row items-center justify-center gap-4"}
           >
-            <div style={{ fontSize: 30, fontWeight: "bold" }}>
-              {data?.surname}
+            <Avatar style={{ width: 100, height: 100 }} />
+            <div
+              className={
+                "flex w-full flex-col items-start justify-center gap-1"
+              }
+            >
+              <div style={{ fontSize: 30, fontWeight: "bold" }}>
+                {data?.surname}
+              </div>
+              <div style={{ fontSize: 20 }}>{data?.name}</div>
             </div>
-            <div style={{ fontSize: 20 }}>{data?.name}</div>
           </div>
         </div>
       </div>
